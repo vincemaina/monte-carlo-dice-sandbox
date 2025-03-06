713d59615bff4b3f77971bd74882821a239a03a3
Vince Maina <vincemaina@proton.me>
2025-03-06 15:15:54 +0000

    feat: add dice icon to header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,7 +28,13 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased p-10 max-w-[1500px] mx-auto min-h-screen flex flex-col`}
       >
         <div className="flex items-center justify-between mb-5">
-          <h1 className="font-black text-3xl">MONTE CARLO <span className="text-indigo-600">DICE SIMULATION</span></h1>
+          <div className="flex items-center gap-3 text-3xl">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" height={"1em"}>
+              {/*<!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->*/}
+              <path d="M274.9 34.3c-28.1-28.1-73.7-28.1-101.8 0L34.3 173.1c-28.1 28.1-28.1 73.7 0 101.8L173.1 413.7c28.1 28.1 73.7 28.1 101.8 0L413.7 274.9c28.1-28.1 28.1-73.7 0-101.8L274.9 34.3zM200 224a24 24 0 1 1 48 0 24 24 0 1 1 -48 0zM96 200a24 24 0 1 1 0 48 24 24 0 1 1 0-48zM224 376a24 24 0 1 1 0-48 24 24 0 1 1 0 48zM352 200a24 24 0 1 1 0 48 24 24 0 1 1 0-48zM224 120a24 24 0 1 1 0-48 24 24 0 1 1 0 48zm96 328c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-192c0-35.3-28.7-64-64-64l-114.3 0c11.6 36 3.1 77-25.4 105.5L320 413.8l0 34.2zM480 328a24 24 0 1 1 0 48 24 24 0 1 1 0-48z" />
+            </svg>
+            <h1 className="font-black">MONTE CARLO <span className="text-indigo-600">DICE SIMULATION</span></h1>
+          </div>
           <a href="https://github.com/vincemaina/monte-carlo-dice-sandbox" target="_blank" className="button !rounded-full !px-4 text-sm">GitHub Repository</a>
         </div>
         {children}
