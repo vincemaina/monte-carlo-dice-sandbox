1ad6e631d1bc89ffb4ea3bd3d517cd8ea5d10bfd
Vince Maina <vincemaina@proton.me>
2025-03-06 12:37:01 +0000

    refactor: move title into layout component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,8 @@ import { MonteCarlo } from "./monte-carlo";
 
 export default function Home() {
   return (
-    <div className="container p-10 max-w-[1500px] mx-auto">
-      <h1 className="font-black text-xl">Monte Carlo Sandbox</h1>
-      <MonteCarlo
-        numIterations={10000}
-        numDice={100}
-        simulationRate={1}
-      />
+    <div className="flex-grow flex flex-col border border-dashed rounded bg-slate-50">
+      <MonteCarlo />
     </div>
   );
 }
